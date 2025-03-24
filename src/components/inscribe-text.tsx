fc7a5489bb25f81629fdcfd45e2adf5636a6e89a
nikhil478 <nikhilmatta45@gmail.com>
2025-03-24 12:29:44 -0700

    added loader

--- src/components/inscribe-text.tsx
+++ src/components/inscribe-text.tsx
@@ -92,11 +92,33 @@ export default function OpReturnForm({ xpriv }: OpReturnFormProps) {
 
         <div className="flex items-center justify-between">
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center"
             type="submit"
             disabled={isLoading}
           >
-            {isLoading ? 'Sending...' : 'Insribe Text'}
+            {isLoading && (
+              <svg
+                className="animate-spin h-5 w-5 mr-2 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 11-8 8z"
+                ></path>
+              </svg>
+            )}
+            {isLoading ? 'Sending...' : 'Inscribe Text'}
           </button>
         </div>
       </form>
